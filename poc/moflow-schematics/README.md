3ea2af358fab3f668d91170426a8536ddb707328
Phodal HUANG <h@phodal.com>
2019-03-29 17:47:04 +0800

    poc: [schematics] init package

--- poc/moflow-schematics/README.md
+++ poc/moflow-schematics/README.md
@@ -5,3 +5,8 @@
 npm link ../moflow-schematics/
 yarn ng g moflow-schematics:moflow-schematics --name "fafa"
 ```
+
+References:
+
+ - https://github.com/oktadeveloper/okta-angular-schematics-example
+ - https://github.com/Robinyo/ionic-angular-schematics
--- poc/moflow-schematics/src/moflow-schematics/index.ts
+++ poc/moflow-schematics/src/moflow-schematics/index.ts
@@ -1,11 +1,22 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { join, normalize } from '@angular-devkit/core';
 
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
+export function setupOptions(host: Tree, options: any): Tree {
+  const workspace = getWorkspace(host);
+  if (!options.project) {
+    options.project = Object.keys(workspace.projects)[0];
+  }
+  const project = workspace.projects[options.project];
+  options.path = join(normalize(project.root), 'src');
+  return host;
+}
+
 export function moflowSchematics(_options: any): Rule {
-  console.log(_options.name);
   return (tree: Tree, _context: SchematicContext) => {
+    setupOptions(tree, _options);
+
     return tree;
   };
 }
