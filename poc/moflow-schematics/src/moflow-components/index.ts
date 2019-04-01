fbfe326875e3a7fea7a806259a7b9389d05b0b5e
Phodal HUANG <h@phodal.com>
2019-04-01 11:00:03 +0800

    poc: [schematics] add css code for test

--- poc/moflow-schematics/src/moflow-components/index.ts
+++ poc/moflow-schematics/src/moflow-components/index.ts
@@ -10,9 +10,11 @@ import {chain, noop, Rule, Tree} from '@angular-devkit/schematics';
 import {
   addModuleImportToModule,
   buildComponent,
-  findModuleFromOptions,
+  findModuleFromOptions, getProjectFromWorkspace, getProjectStyleFile,
 } from '@angular/cdk/schematics';
 import {Schema} from './schema';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { italic, red } from '@angular-devkit/core/src/terminal';
 
 /**
  * Scaffolds a new dashboard component.
@@ -24,7 +26,8 @@ export default function(options: Schema): Rule {
       template: './__path__/__name@dasherize@if-flat__/__name@dasherize__.component.html',
       stylesheet: './__path__/__name@dasherize@if-flat__/__name@dasherize__.component.__styleext__',
     }),
-    options.skipImport ? noop() : addNavModulesToModule(options)
+    options.skipImport ? noop() : addNavModulesToModule(options),
+    appendHtml(options)
   ]);
 }
 
@@ -38,3 +41,41 @@ function addNavModulesToModule(options: Schema) {
     return host;
   };
 }
+
+function appendHtml(options: Schema): Rule {
+  return (host: Tree) => {
+    const workspace = getWorkspace(host);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    const styleFilePath = getProjectStyleFile(project);
+
+    if (!styleFilePath) {
+      console.warn(red(`Could not find the default style file for this project.`));
+      console.warn(red(`Please consider manually setting up the Roboto font in your CSS.`));
+      return;
+    }
+
+    const buffer = host.read(styleFilePath);
+
+    if (!buffer) {
+      console.warn(red(`Could not read the default style file within the project ` +
+        `(${italic(styleFilePath)})`));
+      console.warn(red(`Please consider manually setting up the Robot font.`));
+      return;
+    }
+
+    const htmlContent = buffer.toString();
+    const insertion = '\n' +
+      `html, body { height: 100%; }\n` +
+      `body { margin: 0; font-family: Roboto, "Helvetica Neue", sans-serif; }\n`;
+
+    if (htmlContent.includes(insertion)) {
+      return;
+    }
+
+    const recorder = host.beginUpdate(styleFilePath);
+
+    recorder.insertLeft(htmlContent.length, insertion);
+    host.commitUpdate(recorder);
+  };
+}
+
